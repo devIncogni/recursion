2be2b5180dec896be598931c820508e3c07a1cb3
devIncogni <arinkumar10@outlook.com>
2025-05-12 01:11:43 +0530

    Wrote a function to generate fibo seq using recursion

--- fibonacci.js
+++ fibonacci.js
@@ -16,3 +16,22 @@ function fibs(n) {
 
 let fibArr = fibs(16);
 console.log(fibArr);
+
+// Recursive approach
+
+function fibsRec(n) {
+  if (n === 0) {
+    return [0];
+  } else if (n === 1) {
+    return [0, 1];
+  } else {
+    let fibArr = [];
+    fibArr = fibArr.concat(fibsRec(n - 1));
+    fibArr.push(fibArr[fibArr.length - 1] + fibArr[fibArr.length - 2]);
+  }
+
+  return fibArr;
+}
+
+fibArr = fibsRec(16);
+console.log(fibArr);
